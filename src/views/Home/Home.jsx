e36d34bcdd1e726ac36ff333e0403eda2c99253d
Efix JG <efixjg@Konrads-MacBook-Air.local>
2017-10-28 22:17:17 +0200

    Old changrs done by FX

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Container } from '../../utils/styledComponents';
 import { Header } from './Home_styles';
+import { SearchBar }  from './SearchBar/search_bar';
 
 export default class Home extends Component {
   render() {
     return (
       <Container>
-        <Header>Home page</Header>
+        <Header>Home pagee</Header>
+        <SearchBar/>
       </Container>
     );
   }
